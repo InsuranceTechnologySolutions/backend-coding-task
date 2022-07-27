42196b17b731e41335228dff2fed309eab676207
Stig Nielsen <stig.nielsen@instech.no>
2022-07-27 13:53:31 +0200

    1st commit

---
+++ API/Claims/Cosmos/ICosmosDbService.cs
@@ -0,0 +1,10 @@
+﻿namespace Claims.Cosmos
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Claim>> GetClaimsAsync();
+        Task<Claim> GetClaimAsync(string id);
+        Task AddItemAsync(Claim item);
+        Task DeleteItemAsync(string id);
+    }
+}
